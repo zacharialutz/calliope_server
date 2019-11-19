f4efa44570f599462a2bb6069ac10b43c8d876e8
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-11-19 16:15:36 -0600

    provides GET to client

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	PORT: process.env.PORT || 3000,
+	PORT: process.env.PORT || 8000,
+	CLIENT_ORIGIN: 3000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
-	DB_URL: process.env.DB_URL || 'postgresql://zacharialutz@localhost/wordbank'
+	DB_URL: process.env.DB_URL || 'postgresql://zacharialutz@localhost/wordbank',
+	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT, DB_URL } = require('./config');
 
 const db = knex({
 	client: 'pg',
