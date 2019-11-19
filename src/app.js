c67ea64c995c5feb1d4b0c03097f293cf3a6f145
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-11-19 11:01:31 -0600

    preparing for deployment

--- src/app.js
+++ src/app.js
@@ -24,8 +24,7 @@ app.use(function errorHandler(error, req, res, next) {
 	if (NODE_ENV === 'production') {
 		response = { error: { message: 'server error' } };
 	} else {
-		console.error(error);
-		response = { message: error.message, error };
+		response = { error };
 	};
 	res.status(500).json(response);
 })
--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const app = require('./app')
 const { PORT } = require('./config')
 
+app.get('/api/*', (req, res) => {
+	res.json({ok: true});
+  });
+
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+module.exports = {app};
