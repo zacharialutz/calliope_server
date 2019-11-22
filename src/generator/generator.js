d5c06e65f27040999c5b352072d115a9b612fd40
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-11-22 11:43:06 -0600

    tidied up a bit of code

--- src/generator/generator.js
+++ src/generator/generator.js
@@ -1,18 +1,5 @@
 const WordService = require('./word-service')
 
-const demoStories = [
-	'The quick brown fox jumped over the lazy dog.',
-	'This is the best of stories. This is the worst of stories.',
-	'For sale: baby shoes, never worn.',
-	'Once upon a time there was a test story.',
-	'My very eager mother just served us nine pizzas.',
-	'Rubber baby buggy bumpers',
-	'Winner winner, chicken dinner!',
-	'This is a test. A test it is. Test test test.',
-	'The question is: how far down the rabbit hole do you want to go?',
-	'The more things change, the more they stay the same.'
-]
-
 // Returns a random integer from 1 to (num)
 function roll(num) {
 	return Math.floor( (Math.random() * num) ) + 1;
@@ -25,6 +12,7 @@ function a (str) {
 	else return `a ${str}`;
 }
 
+// Generates items to insert into modular template ----------------
 async function template(db) {
 	// Generate genre
 	let genre;
@@ -52,6 +40,7 @@ async function template(db) {
 		)
 			.then(val => person1 += ` ${val}`);
 
+	// Generate location/setting
 	let location;
 	let locationPrep = 'in';
 		if (roll(3) === 1) {
@@ -64,10 +53,18 @@ async function template(db) {
 				});
 		}
 		else {
-			await WordService.getAdjective(
-				db, 'place'
-			)
-				.then(val => location = val);
+			if (roll(3) === 1) {
+				await WordService.getAdjective(
+					db, 'general'
+				)
+					.then(val => location = val);
+			}
+			else {
+				await WordService.getAdjective(
+					db, 'place'
+				)
+					.then(val => location = val);
+			}
 			await WordService.getSetting(
 				db, 'setting'
 			)
@@ -78,21 +75,17 @@ async function template(db) {
 				});
 		}
 
+	// Generate time setting
 	let period;
 		await WordService.getNoun(
 			db, 'singular', 'period'
 		)
 		.then(val => period = val);
-	// ----------------------------------------------------
+
+	// Fills in template slots to create final story
 	let story = `This ${genre} is about ${a(person1)}. It takes place ${locationPrep} ${location} during ${period}.`;
 	// console.log(story);
 	return story;
-}
-
-// Returns a random item from demo array
-function randomDemo() {
-	const pick = roll(demoStories.length);
-	return demoStories[pick - 1];
 }
 
 // Returns array of stories
@@ -101,7 +94,7 @@ async function generate(db, num = 1) {
 	for (let i = 0; i < num; i++) {
 		await template(db).then(story => list.push(story));
 	}
-	console.log(list);
+	// console.log(list);
 	return list;
 }
 
